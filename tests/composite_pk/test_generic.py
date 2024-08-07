07bc88c05911f3617f43937686b1762be126b154
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-08-07 17:26:06 +0800

    Refs #373 - Add test

--- tests/composite_pk/test_generic.py
+++ tests/composite_pk/test_generic.py
@@ -13,31 +13,27 @@ class CompositePKGenericTests(TestCase):
     def setUpTestData(cls):
         cls.tenant_1 = Tenant.objects.create()
         cls.tenant_2 = Tenant.objects.create()
-        cls.tenant_3 = Tenant.objects.create()
         cls.user_1 = User.objects.create(tenant=cls.tenant_1, id=1)
         cls.user_2 = User.objects.create(tenant=cls.tenant_1, id=2)
-        cls.user_3 = User.objects.create(tenant=cls.tenant_2, id=3)
-        cls.user_4 = User.objects.create(tenant=cls.tenant_3, id=4)
         cls.comment_1 = Comment.objects.create(id=1, user=cls.user_1)
         cls.comment_2 = Comment.objects.create(id=2, user=cls.user_1)
-        cls.comment_3 = Comment.objects.create(id=3, user=cls.user_2)
-        cls.comment_4 = Comment.objects.create(id=4, user=cls.user_3)
-        cls.comment_5 = Comment.objects.create(id=5, user=cls.user_1)
         cls.post_1 = Post.objects.create(tenant=cls.tenant_1, id=UUID(cls.POST_1_ID))
         cls.tag_1 = Tag.objects.create(name="a", content_object=cls.comment_1)
         cls.tag_2 = Tag.objects.create(name="b", content_object=cls.post_1)
         cls.comment_ct = ContentType.objects.get_for_model(Comment)
         cls.post_ct = ContentType.objects.get_for_model(Post)
+        cls.post_1_fk = f'[{cls.tenant_1.id}, "{cls.POST_1_ID}"]'
+        cls.comment_1_fk = f"[{cls.tenant_1.id}, {cls.comment_1.id}]"
 
     def test_fields(self):
         tag_1 = Tag.objects.get(pk=self.tag_1.pk)
         self.assertEqual(tag_1.content_type, self.comment_ct)
-        self.assertEqual(tag_1.object_id, f"[{self.tenant_1.id}, {self.comment_1.id}]")
+        self.assertEqual(tag_1.object_id, self.comment_1_fk)
         self.assertEqual(tag_1.content_object, self.comment_1)
 
         tag_2 = Tag.objects.get(pk=self.tag_2.pk)
         self.assertEqual(tag_2.content_type, self.post_ct)
-        self.assertEqual(tag_2.object_id, f'[{self.tenant_1.id}, "{self.POST_1_ID}"]')
+        self.assertEqual(tag_2.object_id, self.post_1_fk)
         self.assertEqual(tag_2.content_object, self.post_1)
 
         post_1 = Post.objects.get(pk=self.post_1.pk)
@@ -74,7 +70,7 @@ class CompositePKGenericTests(TestCase):
 
         tag_3 = Tag.objects.get(name="c")
         self.assertEqual(tag_3.content_type, self.post_ct)
-        self.assertEqual(tag_3.object_id, f'[{self.tenant_1.id}, "{self.POST_1_ID}"]')
+        self.assertEqual(tag_3.object_id, self.post_1_fk)
         self.assertEqual(tag_3.content_object, post_1)
 
     def test_tags_add(self):
@@ -88,7 +84,7 @@ class CompositePKGenericTests(TestCase):
 
         tag_3 = Tag.objects.get(name="c")
         self.assertEqual(tag_3.content_type, self.post_ct)
-        self.assertEqual(tag_3.object_id, f'[{self.tenant_1.id}, "{self.POST_1_ID}"]')
+        self.assertEqual(tag_3.object_id, self.post_1_fk)
         self.assertEqual(tag_3.content_object, post_1)
 
         tag_4 = Tag.objects.create(name="d", content_object=self.comment_2)
@@ -98,7 +94,7 @@ class CompositePKGenericTests(TestCase):
 
         tag_4 = Tag.objects.get(name="d")
         self.assertEqual(tag_4.content_type, self.post_ct)
-        self.assertEqual(tag_4.object_id, f'[{self.tenant_1.id}, "{self.POST_1_ID}"]')
+        self.assertEqual(tag_4.object_id, self.post_1_fk)
         self.assertEqual(tag_4.content_object, post_1)
 
     def test_tags_set(self):
@@ -120,5 +116,19 @@ class CompositePKGenericTests(TestCase):
         tag_3, created = post_1.tags.get_or_create(name="c")
         self.assertTrue(created)
         self.assertEqual(tag_3.content_type, self.post_ct)
-        self.assertEqual(tag_3.object_id, f'[{self.tenant_1.id}, "{self.POST_1_ID}"]')
+        self.assertEqual(tag_3.object_id, self.post_1_fk)
+        self.assertEqual(tag_3.content_object, post_1)
+
+    def test_tags_update_or_create(self):
+        post_1 = Post.objects.get(pk=self.post_1.pk)
+
+        tag_2, created = post_1.tags.update_or_create(name="b", defaults={"name": "b2"})
+        self.assertFalse(created)
+        self.assertEqual(tag_2, self.tag_2)
+        self.assertEqual(tag_2.name, "b2")
+
+        tag_3, created = post_1.tags.update_or_create(name="c")
+        self.assertTrue(created)
+        self.assertEqual(tag_3.content_type, self.post_ct)
+        self.assertEqual(tag_3.object_id, self.post_1_fk)
         self.assertEqual(tag_3.content_object, post_1)
